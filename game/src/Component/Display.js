9389a76723d87fa09a800865035f009870e6a01f
Manju KC <kcmanjuthapa@gmail.com>
2019-10-03 15:25:56 -0400

    created Display function

--- game/src/Component/Display.js
+++ game/src/Component/Display.js
@@ -3,7 +3,10 @@ import React from "react";
 const Display = props => {
     return(
         <div>
-            
+            <h2>{props.game.balls}</h2>
+            <h2>{props.game.strikes}</h2>
         </div>
     )
 }
+
+export default Display;

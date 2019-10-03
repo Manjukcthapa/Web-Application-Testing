e9c1864da6e3807d63fa6745e0e3aebc4e3b6f26
Manju KC <kcmanjuthapa@gmail.com>
2019-10-03 16:02:19 -0400

    added handleBall function into app.js

--- game/src/App.js
+++ game/src/App.js
@@ -1,22 +1,38 @@
-import React, { Component } from 'react';
-import Display from "./Component/Display"
-import Dashboard from "./Component/Dashboard"
+import React, { Component } from "react";
+import Display from "./Component/Display";
+import Dashboard from "./Component/Dashboard";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      balls:0,
-      strikes:0
+      balls: 0,
+      strikes: 0
     };
   }
+
+  handleReset = e => {
+    this.setState({
+      balls: 0,
+      strikes: 0
+    });
+  };
+
+  handleBall = e => {
+    if (this.state.balls < 3) {
+      this.setState(prevState => {
+        return { balls: prevState.balls + 1 };
+      });
+    } else this.handleReset(e);
+  };
+
   render() {
     return (
-      <div >
-        <Display game={this.state}/>
-        <Dashboard/>
+      <div>
+        <Display reset={handleReset} game={this.state} />
+        <Dashboard onBall={this.handleBall} />
       </div>
     );
   }
